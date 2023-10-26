b4b2f4c136b0af1608fc9f46b107f09fa3b6a419
inomoto daiya <daiandaian2525@gmail.com>
2023-10-26 23:18:31 +0900

    feat: Create component props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,9 @@
+@props([
+    'title',
+    'content' => '初期値です。',
+    'message' => '本文初期値です。'
+])
+
 <div class="border-2 shadow-md w-1/4 p-2">
     <div class="text-white">{{ $title }}</div>
     <div class="text-white">画像</div>
