3b935470916a2eab861f861df18c9246aa3ac515
inomoto daiya <daiandaian2525@gmail.com>
2023-10-27 00:39:25 +0900

    feat: Create component class

---
+++ resources/views/components/tests/test-class-base-component.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <p class="font-bold text-white text-xl">クラスベースのコンポーネント</p>
+    <div class="text-white"> {{ $classBaseMessage }}</div>
+    <div class="text-white">{{ $defaultMessage }}</div>
+    <!-- You must be the change you wish to see in the world. - Mahatma Gandhi -->
+</div>
--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -3,4 +3,9 @@
         <p class="dark:text-white">ヘッダー2</p>
     </x-slot>
     <p class="dark:text-white">コンポーネントテスト2</p>
+    <div class="mb-4"></div>
+
+    <x-test-class-base classBaseMessage="メッセージです"/>
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージです" defaultMessage="初期値から変更しています"/>
 </x-tests.app>
