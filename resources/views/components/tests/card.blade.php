d91c954d2c0c2f35a56bf2c2f544c65cbb71e92a
inomoto daiya <daiandaian2525@gmail.com>
2023-10-26 23:07:49 +0900

    feat: Create component props

---
+++ resources/views/components/tests/card.blade.php
@@ -0,0 +1,5 @@
+<div class="border-2 shadow-md w-1/4 p-2">
+    <div class="text-white">{{ $title }}</div>
+    <div class="text-white">画像</div>
+    <div class="text-white">{{ $content }}</div>
+</div>
